cf125689dfd48fc26361ac595dfa22a561d68c18
EGavinG <edwardgavingarcia@yahoo.com>
2021-01-05 23:25:06 -0600

    JS and Index change

--- js/casestudy.js
+++ js/casestudy.js
@@ -25,13 +25,11 @@ $(document).ready(function () {
     );
   });
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  mybutton = document.getElementById("myBtn");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
@@ -44,6 +42,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-var carouselExampleDark = document.querySelector('#carouselExampleDark')
-var carousel = new bootstrap.Carousel(myCarousel)
